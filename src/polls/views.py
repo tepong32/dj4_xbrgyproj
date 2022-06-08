b7651743e47bce8d86424093db5331a51f511bf9
tepong32 <tepong32@gmail.com>
2022-06-09 00:14:30 +0800

    worked until detail view

--- src/polls/views.py
+++ src/polls/views.py
@@ -1,11 +1,33 @@
+### Leaving this here as an example on how the 'render' shortcut works
+### See below for the re-written 'index()'' view
 from django.http import HttpResponse
+# from django.template import loader
+# from .models import *
+
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
--- src/src/settings.py
+++ src/src/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g0nyabj=_k%6%%nfq$%=m@9z0i+m_kwx1%y4cs*@)z)rh59+6*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
